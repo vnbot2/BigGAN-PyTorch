dc127f8e695ceb9013edd4f120283b36841c070c
Anh Vo Tran Hai <vnbot2@gmail.com>
2019-11-01 01:38:59 +0000

    k

--- run_train.sh
+++ run_train.sh
@@ -2,8 +2,8 @@
 python train.py \
 --data_root downloads/motobike \
 --experiment_name generative_motobike \
---num_epochs 2000 --shuffle --num_workers 2 --batch_size 2048 \
---num_D_steps 1 --G_lr 1e-4 --D_lr 4e-4 --D_B2 0.999 --G_B2 0.999 \
+--num_epochs 5000 --shuffle --num_workers 16 --batch_size 2000 \
+--num_D_steps 1 --G_lr 2e-4 --D_lr 8e-4 --D_B2 0.999 --G_B2 0.999 \
 --G_ch 32 --D_ch 64 \
 --G_attn 32 --D_attn 32 \
 --G_nl inplace_relu --D_nl inplace_relu \
@@ -14,6 +14,7 @@ python train.py \
 --G_init ortho --D_init ortho \
 --G_eval_mode \
 --ema --use_ema --ema_start 30000 \
---save_every 5000 --sample_every 50 --log_interval 1 --num_fixed_samples 9 \
- --num_save_copies 0 --seed 1234 \
+--save_every 1000 --sample_every 50 --log_interval 1 --num_fixed_samples 25 \
+ --num_save_copies 2 --seed 1234 \
  --load_in_mem \
+ --resume
--- train_fns.py
+++ train_fns.py
@@ -119,11 +119,10 @@ def save_and_sample(G, D, G_ema, z_, y_, fixed_z, fixed_y,
             fixed_Gz = nn.parallel.data_parallel(which_G, (fixed_z, which_G.shared(fixed_y)))
         else:
             fixed_Gz = which_G(fixed_z, which_G.module.shared(fixed_y))
-    if not os.path.isdir('%s/%s' % (config['samples_root'], experiment_name)):
-        os.mkdir('%s/%s' % (config['samples_root'], experiment_name))
     image_filename = '%s/%s/fixed_samples%d.jpg' % (config['samples_root'],
                                                     experiment_name,
                                                     state_dict['itr'])
+    os.makedirs(os.path.dirname(image_filename), exist_ok=True)
     torchvision.utils.save_image(fixed_Gz.float().cpu(), image_filename,
                                  nrow=int(fixed_Gz.shape[0] ** 0.5), normalize=True)
     # For now, every time we save, also save sample sheets
@@ -135,15 +134,18 @@ def save_and_sample(G, D, G_ema, z_, y_, fixed_z, fixed_y,
                        experiment_name=experiment_name,
                        folder_number=state_dict['itr'],
                        z_=z_)
-    # Also save interp sheets
-    for fix_z, fix_y in zip([False, False, True], [False, True, False]):
-        utils.interp_sheet(which_G,
-                           num_per_sheet=16,
-                           num_midpoints=8,
-                           num_classes=config['n_classes'],
-                           parallel=config['parallel'],
-                           samples_root=config['samples_root'],
-                           experiment_name=experiment_name,
-                           folder_number=state_dict['itr'],
-                           sheet_number=0,
-                           fix_z=fix_z, fix_y=fix_y, device='cuda')
+    # if not os.path.isdir('%s/%s' % (config['samples_root'], experiment_name)):
+    #     os.mkdir('%s/%s' % (config['samples_root'], experiment_name))
+    
+    # # Also save interp sheets
+    # for fix_z, fix_y in zip([False, False, True], [False, True, False]):
+    #     utils.interp_sheet(which_G,
+    #                        num_per_sheet=16,
+    #                        num_midpoints=8,
+    #                        num_classes=config['n_classes'],
+    #                        parallel=config['parallel'],
+    #                        samples_root=config['samples_root'],
+    #                        experiment_name=experiment_name,
+    #                        folder_number=state_dict['itr'],
+    #                        sheet_number=0,
+    #                        fix_z=fix_z, fix_y=fix_y, device='cuda')
